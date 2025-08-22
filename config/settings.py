95a9f81de3202f119d4a4be5247c2b29300fbe54
Chukseo <164390660+Chukseo@users.noreply.github.com>
2025-08-22 04:19:10 +0100

    payment deployment

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("SECRET_KEY", "dev-unsafe")
 DEBUG = os.getenv("DEBUG", "True") == "True"
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost,https://eventback1.onrender.com").split(",")
 
 INSTALLED_APPS = [
     "django.contrib.admin",
